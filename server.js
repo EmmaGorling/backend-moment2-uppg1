40fb34d2524af6c9744b256a96c014799ab993d8
Emma Ådahl Görling <143135641+EmmaGorling@users.noreply.github.com>
2024-04-11 14:50:58 +0200

    Försökte ansluta till phpMyAdmin istället och nu funkar det inte... Åker och jobbar istället.....

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-const {Client} = require('pg');
+const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-const client = new Client ({
+app.use(cors());
+app.use(express.json());
+
+//  Anslut till databas
+const connection = mysql.createConnection({
     host: process.env.HOST,
-    port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    database: process.env.DATABASE
 });
+connection.connect((err) => {
+    if(err) {
+        console.log('Fel vid anslutning.. ' + err);
+        return;
+    }
 
-app.use(cors());
+    console.log('Ansluten till databasen');
+})
+
 
 // Routes
 
@@ -23,7 +30,7 @@ app.use(cors());
 app.get('/api', (req, res) => {
     res.json({message: 'Välkommen'});
 });
-
+// Hämta arbetsefarenheter
 app.get('/api/workexperience', (req, res) => {
     res.json({message:'Hämta arbetserfarenheter'}); 
 });
@@ -36,6 +43,10 @@ app.post('/api/workexperience', (req, res) => {
     let startdate = req.body.startdate;
     let enddate = req.body.enddate;
     let description = req.body.description;
+
+    if(!companyname || !jobtitle || !location || !startdate || !enddate || !description) {
+        res.json({error: 'Mata in all information'}); 
+    }
 
 
     res.json({message:'Lägg till arbetserfarenheter'}); 
